c6ec6abc20d36f0fcf9782cb0175dc69b00f590b
wxia <qlhdtly12189@163.com>
2020-11-08 22:14:23 +0800

    apps:server 去除不必要引入的模块

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,14 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FriendModule } from './friend/friend.module';
-import { MessageController } from './message/message.controller';
-import { MessageService } from './message/message.service';
 import { MessageModule } from './message/message.module';
-import { AuthModule } from 'libs/auth';
 
 @Module({
-  imports: [DbModule, UsersModule, FriendModule, MessageModule, AuthModule],
-  controllers: [AppController, MessageController],
-  providers: [AppService, MessageService]
+  imports: [DbModule, UsersModule, FriendModule, MessageModule],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule {}
