742c5b3030a4ac82da5698753a9aa1bceb188abe
wxia <qlhdtly12189@163.com>
2020-11-08 22:16:12 +0800

    apps:server users模块引入AuthModule

--- apps/server/src/users/users.module.ts
+++ apps/server/src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'libs/auth';
 import { UsersController } from './users.controller';
 import { Users } from './users.entity';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users]), AuthModule],
   controllers: [UsersController],
   providers: [UsersService]
 })
