de255bcd70ba71c52ead7745412e8b3bdef2690d
wxia <qlhdtly12189@163.com>
2020-11-13 23:17:31 +0800

    apps:server friends 在friend.module中引入Users实体

--- apps/server/src/modules/friends/friend.module.ts
+++ apps/server/src/modules/friends/friend.module.ts
@@ -4,9 +4,10 @@ import { FriendController } from './friend.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Proposers } from '../../emtites/friends/proposers.emtity';
 import { AuthModule } from 'libs/auth';
+import { Users } from '../../emtites/users/users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Proposers]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Proposers, Users]), AuthModule],
   providers: [FriendService],
   controllers: [FriendController]
 })
