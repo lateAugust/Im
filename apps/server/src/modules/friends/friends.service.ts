66666040194a59a378dbf26691747c047dc4771c
wxia <qlhdtly12189@163.com>
2020-11-18 01:14:36 +0000

    apps:server friends searching完成

--- apps/server/src/modules/friends/friends.service.ts
+++ apps/server/src/modules/friends/friends.service.ts
@@ -17,21 +17,19 @@ export class FriendsService {
   }
   friendsList() {}
   async searching({ page, page_size, keywords }: FriendsSearchingDto, id: number): Promise<ReturnBody<Users | []>> {
-    page = Number(page);
-    page_size = Number(page_size);
-    if (!keywords) {
-      return { status: false, statusCode: 400, message: '关键词是必须的', data: [] };
-    }
+    page = Number(page || 1);
+    page_size = Number(page_size || 10);
     /* let sql = `SELECT DISTINCT users.id,users.username,users.nickname,users.mobile,users.gender,users.age,users.avatar,users.address
     FROM users LEFT JOIN friends
     ON users.id <> friends.relation_id
     WHERE users.id<>${id}
     AND (instr(users.username, '${keywords}') > 0 OR instr(users.mobile, '${keywords}') > 0)
     ORDER BY users.id LIMIT ${Math.max(0, page - 1) * page_size},${page_size}`; */
-    let sql = `SELECT SQL_CALC_FOUND_ROWS id,username,nickname,gender,age,address,mobile,avatar,email,create_at
-      FROM users 
-      WHERE (instr(users.username, '${keywords}') > 0 OR instr(users.mobile, '${keywords}') > 0) 
+    let sql = `SELECT SQL_CALC_FOUND_ROWS id,username,nickname,gender,age,address,mobile,avatar,email,create_at FROM users`;
+    if (keywords) {
+      sql += ` WHERE (instr(users.username, '${keywords}') > 0 OR instr(users.mobile, '${keywords}') > 0) 
       ORDER BY users.id LIMIT ${Math.max(0, page - 1) * page_size},${page_size}`;
+    }
     let result = await this.usersRepositotry.query(sql);
     let totalResult = await this.usersRepositotry.query('SELECT FOUND_ROWS()');
     let total = totalResult[0]['FOUND_ROWS()'] * 1;
