e3b42b7acaa9671039cfc04b63ac7759bad9043f
wxia <qlhdtly12189@163.com>
2020-11-21 09:57:58 +0000

    apps:server utils dataType类型判断

--- apps/server/src/utils/utils.ts
+++ apps/server/src/utils/utils.ts
@@ -18,3 +18,23 @@ export function updateValues(object: object): string {
 export function pagination(page: number, page_size: number): string {
   return ` ${Math.max(0, page - 1) * page_size},${page_size}`;
 }
+
+export function dataType(v: any): string {
+  let type = {
+    '[object String]': 'string',
+    '[object Number]': 'number',
+    '[object Boolean]': 'boolean',
+    '[object Symbol]': 'Symbol',
+    '[object Undefined]': 'undefined',
+    '[object Null]': 'null',
+    '[object Function]': 'function',
+    '[object Date]': 'date',
+    '[object Array]': 'array',
+    '[object RegExp]': 'regexp',
+    '[object Object]': 'object',
+    '[object Error]': 'error',
+    '[object HTMLDocument]': 'htmlDocument',
+    '[object Window]': 'window'
+  };
+  return type[Object.prototype.toString.call(v)];
+}
