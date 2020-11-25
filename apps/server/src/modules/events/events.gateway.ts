a33cd6b3a6b146e7ad209ebc17ec533d706a422d
wxia <qlhdtly12189@163.com>
2020-11-25 02:01:43 +0000

    apps:server events 数据库引入

--- apps/server/src/modules/events/events.gateway.ts
+++ apps/server/src/modules/events/events.gateway.ts
@@ -1,16 +1,31 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import { SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Server } from 'ws';
+import { Repository } from 'typeorm';
+import WebSocket, { Server } from 'ws';
+import { Links } from '../../emtites/events/links.emtity';
 
 @WebSocketGateway(8080)
 export class EventsGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('events')
-  onEvent(client: any, data: any): Observable<WsResponse<number>> {
-    console.log(this.server);
-    return from([1]).pipe(map(item => ({ event: 'events', data: item })));
+  constructor(@InjectRepository(Links) private readonly linksRepository: Repository<Links>) {}
+
+  @SubscribeMessage('message')
+  onEvent(client: WebSocket, data: any): Observable<WsResponse<number>> {
+    console.log(data);
+    // this.linksRepository.save({ message: '88888' });
+    return from([1]).pipe(map(item => ({ event: 'message', data: item })));
+  }
+  handleConnection(client) {
+    // console.log(client);
+    // 初始化连接
+  }
+
+  handleDisconnect(client: WebSocket) {
+    console.log(client);
+    // 断开
   }
 }
--- apps/server/src/modules/events/events.module.ts
+++ apps/server/src/modules/events/events.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Links } from '../../emtites/events/links.emtity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Links])],
   providers: [EventsGateway]
 })
 export class EventsModule {}
