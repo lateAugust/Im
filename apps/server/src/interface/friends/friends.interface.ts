36cd2f592414f3ecf9a978c21076d92ddea05cda
wxia <qlhdtly12189@163.com>
2021-01-26 22:17:44 +0800

    apps:server interface friends  字段调整和添加好友列表和详情字段添加

--- apps/server/src/interface/friends/friends.interface.ts
+++ apps/server/src/interface/friends/friends.interface.ts
@@ -1,4 +1,5 @@
-export interface FriendsSearchingInterface {
+// Raw
+interface UsersBaseRaw {
   user_id: number;
   user_username: string;
   user_pin_yin: string;
@@ -6,32 +7,88 @@ export interface FriendsSearchingInterface {
   user_age: number | null;
   user_gender: number | null;
   user_avatar: string | null;
+}
+interface FriendsBaseRaw {
   frined_id: number | null;
+}
+
+interface FriendsRaw {
+  fruebd_agree_id: number;
+  friend_relation_id: number;
+  friend_contact_id: number;
+  friend_apply_id: number;
+  friend_update_at: string;
+  friend_create_at: string;
+}
+
+interface ProposerBaseRaw {
   proposer_id: number | null;
   proposer_message: string | null;
   proposer_apply_status: string | null;
   proposer_target_id: number | null;
 }
+export interface FriendsSearchingInterface extends UsersBaseRaw, FriendsBaseRaw, ProposerBaseRaw {}
 
+export interface FriendsListInterface extends UsersBaseRaw, FriendsBaseRaw, FriendsRaw {}
+
+export interface FriendsListDetailRawInterface extends UsersBaseRaw, FriendsBaseRaw, FriendsRaw {
+  mobile: string | null;
+  address: string | null;
+  email: string | null;
+  create_at: string | null;
+}
+
+// body
+
+interface UsersBaseBody {
+  id: number;
+  username: string;
+  pin_yin: string;
+  nickname: string | null;
+  age: number | null;
+  avatar: string | null;
+  gender: string | null;
+}
+
+interface FriendsBaseBody<T> {
+  id: T;
+}
+
+interface FriendsBody extends FriendsBaseBody<number> {
+  agree_id: number;
+  relation_id: number;
+  contact_id: number;
+  apply_id: number;
+  update_at: string;
+  create_at: string;
+}
+
+interface ProposerBaseBody {
+  id: number | null;
+  message: string | null;
+  apply_status: string | null;
+  target_id: number | null;
+}
 export interface FriendsSearchingBodyInterface {
-  friend: {
-    id: number | null;
-  };
-  user: {
-    id: number;
-    username: string;
-    pin_yin: string;
-    nickname: string | null;
-    age: number | null;
-    avatar: string | null;
-    gender: string | null;
-  };
-  proposer: {
-    id: number | null;
-    message: string | null;
-    apply_status: string | null;
-    target_id: number | null;
-  };
+  friend: FriendsBaseBody<number | null>;
+  user: UsersBaseBody;
+  proposer: ProposerBaseBody;
+}
+
+export interface FriendsListBodyInterface {
+  friend: FriendsBody;
+  user: UsersBaseBody;
+}
+
+interface FriendsDetailUsers extends UsersBaseBody {
+  mobile: string | null;
+  email: string | null;
+  address: string | null;
+  create_at: string;
+}
+export interface FriendsListDetailInterFace {
+  friend: FriendsBody;
+  user: FriendsDetailUsers;
 }
 export interface FriendsApplyCountInterface {
   count: number;
